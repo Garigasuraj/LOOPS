a749263316cc6e9cef1b247442c9ac8c2cd24d00
Gariga suraj <104826351+Garigasuraj@users.noreply.github.com>
2022-05-16 15:31:12 +0530

    Create print_alphabets.java

---
+++ print_alphabets.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        for(char i='a'; i<='z'; i++){
+            System.out.println(i);
+        }
+        //Scanner Sc = new Scanner(System.in);
+        //int a = Sc.nextInt();
+        
+        //Solution obj = new Solution();
+    }
+}
