79243483cba272875247b819cb2e171bca14ebf3
Gariga suraj <104826351+Garigasuraj@users.noreply.github.com>
2022-07-19 15:32:13 +0530

    Create factorial_sum.java
    https://ide.geeksforgeeks.org/d952c1fa-9066-4b8f-8487-5b0a8d632bf8

---
+++ factorial_sum.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class factorial_sum {
+    static void int fact_Sum(int n){
+        int sum=0;
+        while(n>0){
+            int fact=1;
+            int rem = n%10;
+            if(n<10){
+                for(int i=1; i<=n; i++){
+                    fact*= i;
+                }
+            }
+            else{
+                for(int i=1; i<=rem; i++){
+                    fact*=i;
+                }
+            }
+            sum+= fact;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner Sc= new Scanner(System.in);
+        int n = Sc.nextInt();
+        
+        System.out.println(fact_Sum(n));
+    }
+}
